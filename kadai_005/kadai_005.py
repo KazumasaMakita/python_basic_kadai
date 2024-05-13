4a0ab08e1444c518074ac03853da0d083f981002
KazumasaMakita <166221660+KazumasaMakita@users.noreply.github.com>
2024-05-13 12:11:51 +0530

    Update kadai_005.py

--- kadai_005/kadai_005.py
+++ kadai_005/kadai_005.py
@@ -12,4 +12,4 @@ bottom_edge = 20
 height = 5
 
 area_of_trapezoid = ((top_edge + bottom_edge) * height) / 2
-print(f"台形の面積は{area_of_trapezoid}cm^2です。")
+print(f"台形の面積は{area_of_trapezoid}cm\u00b2です。")
